1093526de374cec56cb3dd3807a6e6f96f731a65
joselpereda <joselpereda@hotmail.com>
2023-02-24 20:12:17 -0800

    added 2-coinFlipStreak.js

--- js/1-conFlipRedux.js
+++ js/1-conFlipRedux.js
@@ -5,7 +5,7 @@ let coinFlip;
 let  cycleTimes = parseInt(prompt("How many times do you want to play flip the coin?"));
 
 for (i = 0 ; i < cycleTimes; i++ ) {
-    // Determine the result of the coin toss
+    // Determine the result of the coin toss, 0 or 1
     coinFlip = Math.round(Math.random());
 
     // If coin toss = 0, write "Heads" to the console
@@ -17,3 +17,5 @@ for (i = 0 ; i < cycleTimes; i++ ) {
         console.log("Tails");
     }
 }
+// Write message to indicate program has ended
+console.log("Thank you for playing!");
