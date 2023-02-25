524fdfba4a353de3e5841a9fe324f8f107f8225f
joselpereda <joselpereda@hotmail.com>
2023-02-24 22:09:32 -0800

    corrected program logic

--- js/5-marcoPolo.js
+++ js/5-marcoPolo.js
@@ -3,22 +3,22 @@ let currentNumber = 1;
 let numIterations = 100;
 
 do {
-    // First, check if number is a multiple of 3
+    // First check if number is multipe of 3 and 5
+    // If true, write "Marco! Polo!" to the console
+    if (currentNumber % 3 === 0 && currentNumber % 5 === 0) {
+        console.log("Marco! Polo!");
+    
+    // Second, check if it is a multiple of 3
     // If true, write "Marco!" to the the console
-    if (currentNumber % 3 === 0) {
+    } else if (currentNumber % 3 === 0) {
         console.log("Marco!");
     
-    // Second, check if number is a multiple of 5
+    // Third, check if number is a multiple of 5
     // If true, write "Polo!" to the console
     } else if (currentNumber % 5 === 0) {
         console.log("Polo!");
     
-    // Third, check if number is multipe of 3 and 5
-    // If true, write "Marco! Polo!" to the console
-    } else if (currentNumber % 3 === 0 && currentNumber % 5 === 0) {
-        console.log("Marco! Polo!");
-
-    // Otherwise simply write the current number to the console
+    // else, write the number to the console
     } else {
         console.log(currentNumber);
     }
